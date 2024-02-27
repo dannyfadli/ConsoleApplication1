57f4815f06ff2d86464295bf48b416ee8298d365
dannyfadli <danyfdly95@gmail.com>
2024-02-27 12:29:48 +0700

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{//begin 
+
+	//numeric nAlas, nTinggi, nLuas
+	//display 'masukan Alassnya= '
+	//accept nAlas
+	//display 'masukan Tingginya= '
+	//compute nLuas =1/2 *nAlas * nTinggi
+	//display 'Luasnya adalah= ' + nLuas
+
+	int nAlas, nTinggi;
+	double nLuas;
+	cout << "Masukan Alasnya=";
+	cin >> nAlas;
+	cout << "Masukan Tingginya=";
+	cin >> nTinggi;
+	nLuas = 0.5 * (nAlas * nTinggi);
+	cout << "Luasnya adalah= " << nLuas << endl;
+
+
+	system("pause");
+
+}//end
